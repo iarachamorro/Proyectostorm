511d1d547b1b161f2d1d1a49002217c5e5b4722a
mramirez <mramirez@gmail.com>
2023-06-29 14:35:48 -0300

    terminado

--- js/javascript.js
+++ js/javascript.js
@@ -5,14 +5,15 @@ window.addEventListener("load",function(){
     imagenes[1] = "./img/carrusel2.jpg";
     imagenes[2] = "./img/carrusel3.jpg";
     imagenes[3] = "./img/carrusel4.jpg";
-
+    imagenes[4] = "./img/carrusel5.jpg";
+    imagenes[5] = "./img/carrusel6.jpg";
     var indiceImagenes = 0;
-    var tiempo = 1500;
+    var tiempo = 1000;
 
     function cambiarImagenes(){
         document.getElementsByName("slider")[0].src = imagenes[indiceImagenes];
      
-        if (indiceImagenes < 2){
+        if (indiceImagenes < 5){
             indiceImagenes++;
         } else {
             indiceImagenes = 0;
@@ -20,6 +21,4 @@ window.addEventListener("load",function(){
         }
     }
     this.setInterval(cambiarImagenes, tiempo)
-
-
 });
